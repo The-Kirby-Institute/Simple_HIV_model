7fd9f6b499be901f8199e16450fcf46f90ef16e6
rongxing <rongxingwengcurtis@gmail.com>
2023-06-28 23:19:51 +1000

    codes revised for para values

--- code/Parameters.R
+++ code/Parameters.R
@@ -18,15 +18,15 @@ projectspecs$betaoption<-as.character    (projectspecs$betaoption)
 #summary(data1)
 
 # Put parmaters in a list for beta
-parameters_beta0<- c(S0=fixeddata$value[11],##number of susceptible people
-                     N0=fixeddata$value[1], ##plhiv
-                     I0=fixeddata$value[10], ##new infection
-                     d0=fixeddata$value[2], ##proportion  diagnosed
-                     tau0=fixeddata$value[3], ##proportion on ART
-                     sigma0=fixeddata$value[4] ,##proportion suppressed
-                     phi=defaultpara$value[1],  ##the reduction in transmission due to suppression 0.95?
-                     omega0= fixeddata$value[5], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
-                     epsilon_PrEP=defaultpara$value[2])  ###scott(0.86) efficacy of PrEP
+parameters_beta0<- c(S0=fixeddata$value[fixeddata$Para == "numsus"],##number of susceptible people
+                     N0=fixeddata$value[fixeddata$Para == "plhiv"], ##plhiv
+                     I0=fixeddata$value[fixeddata$Para == "newinf"], ##new infection
+                     d0=fixeddata$value[fixeddata$Para == "prodiag"], ##proportion  diagnosed
+                     tau0=fixeddata$value[fixeddata$Para == "proART"], ##proportion on ART
+                     sigma0=fixeddata$value[fixeddata$Para == "prosupp"] ,##proportion suppressed
+                     phi=defaultpara$value[defaultpara$Para == "effisupp"],  ##the reduction in transmission due to suppression 0.95?
+                     omega0= fixeddata$value[fixeddata$Para == "PrEPcov"], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
+                     epsilon_PrEP=defaultpara$value[defaultpara$Para == "effiPrEP"])  ###scott(0.86) efficacy of PrEP
 
 #different functions for beta0
 # beta option
@@ -41,18 +41,18 @@ if (projectspecs$betaoption == "N") {
 
 # After obtain beta0, Put parmaters as vectors for HIVmodel
 parameters<- c(beta0 =beta0,
-               N0=fixeddata$value[1], ##plhiv
-               I0=fixeddata$value[10], ##new infection
-               d0=fixeddata$value[2], ##proportion  diagnosed
-               tau0=fixeddata$value[3], ##proportion on ART
-               sigma0=fixeddata$value[4] ,##proportion suppressed
-               phi=defaultpara$value[1],  ##the reduction in transmission due to suppression 0.95?
-               omega0= fixeddata$value[5], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
-               epsilon_PrEP=defaultpara$value[2],  ###scott(0.86) efficacy of PrEP
-               epsilon_condom=defaultpara$value[3],  ###scott
-               chi_0=fixeddata$value[12], ##condom use FIG21 HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
-               alpha0=fixeddata$value[13],  ####Table1 Hammoud, total number of partners mean
-               T= fixeddata$value[6])##FIG22a HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
+               N0=fixeddata$value[fixeddata$Para == "plhiv"], ##plhiv
+               I0=fixeddata$value[fixeddata$Para == "newinf"], ##new infection
+               d0=fixeddata$value[fixeddata$Para == "prodiag"], ##proportion  diagnosed
+               tau0=fixeddata$value[fixeddata$Para == "proART"], ##proportion on ART
+               sigma0=fixeddata$value[fixeddata$Para == "prosupp"] ,##proportion suppressed
+               phi=defaultpara$value[defaultpara$Para == "effisupp"],  ##the reduction in transmission due to suppression 0.95?
+               omega0= fixeddata$value[fixeddata$Para == "PrEPcov"], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
+               epsilon_PrEP=defaultpara$value[defaultpara$Para == "effiPrEP"],  ###scott(0.86) efficacy of PrEP
+               epsilon_condom=defaultpara$value[defaultpara$Para == "efficon"],  ###efficacy condoms, scott 
+               chi_0=fixeddata$value[fixeddata$Para == "conduse"], ##proportion of condom use FIG21 HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
+               alpha0=fixeddata$value[fixeddata$Para == "sexact"],  ####proportion of sexual acts (baseline) Table1 Hammoud, total number of partners mean
+               T= fixeddata$value[fixeddata$Para == "testpro"])##proportion of testing, FIG22a HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
 #the monthly data are extracted. (below are monthly data from Jan 2020 - Dec 2020)
 temporal_data<-  as.data.frame(  cbind(alpha_t=c(timeseries$acts),###Table1 Hammoud
                                      chi_t=c(timeseries$condoms), ##FIG21 NSW HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
