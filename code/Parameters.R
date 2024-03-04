4d5b7214ee1700bb55cb6242fd1c959e45aafde0
rongxing <rongxingwengcurtis@gmail.com>
2024-03-04 23:36:46 +1100

    add codes to make all the outputs into one file

--- code/Parameters.R
+++ code/Parameters.R
@@ -1,7 +1,7 @@
 ####This script is to Generate parameters
 ##no need to change the content in this script
-Parameters<-function(defaultpara,fixeddata, timeseries,projectspecs){  
-  with(as.list(c(defaultpara,fixeddata, timeseries,projectspecs)), {
+Parameters<-function(defaultpara,fixeddata, timeseries,projectspecs){
+with(as.list(c(defaultpara,fixeddata, timeseries,projectspecs)), {
 # Setting up parameters
 defaultpara$value<-as.numeric(defaultpara$value)
 fixeddata$value<-as.numeric(fixeddata$value)
@@ -16,57 +16,50 @@ timeseries$Deaths<-as.numeric(timeseries$Deaths)
 projectspecs$betaoption<-as.character    (projectspecs$betaoption)
 #summary(data2)
 #summary(data1)
-
 # Put parmaters in a list for beta
 parameters_beta0<- c(S0=fixeddata$value[fixeddata$Para == "numsus"],##number of susceptible people
-                     N0=fixeddata$value[fixeddata$Para == "plhiv"], ##plhiv
-                     I0=fixeddata$value[fixeddata$Para == "newinf"], ##new infection
-                     d0=fixeddata$value[fixeddata$Para == "prodiag"], ##proportion  diagnosed
-                     tau0=fixeddata$value[fixeddata$Para == "proART"], ##proportion on ART
-                     sigma0=fixeddata$value[fixeddata$Para == "prosupp"] ,##proportion suppressed
-                     phi=defaultpara$value[defaultpara$Para == "effisupp"],  ##the reduction in transmission due to suppression 0.95?
-                     omega0= fixeddata$value[fixeddata$Para == "PrEPcov"], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
-                     epsilon_PrEP=defaultpara$value[defaultpara$Para == "effiPrEP"])  ###scott(0.86) efficacy of PrEP
-
+N0=fixeddata$value[fixeddata$Para == "plhiv"], ##plhiv
+I0=fixeddata$value[fixeddata$Para == "newinf"], ##new infection
+d0=fixeddata$value[fixeddata$Para == "prodiag"], ##proportion  diagnosed
+tau0=fixeddata$value[fixeddata$Para == "proART"], ##proportion on ART
+sigma0=fixeddata$value[fixeddata$Para == "prosupp"] ,##proportion suppressed
+phi=defaultpara$value[defaultpara$Para == "effisupp"],  ##the reduction in transmission due to suppression 0.95?
+omega0= fixeddata$value[fixeddata$Para == "PrEPcov"], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
+epsilon_PrEP=defaultpara$value[defaultpara$Para == "effiPrEP"])  ###scott(0.86) efficacy of PrEP
 #different functions for beta0
 # beta option
 # beta option 1: the number of infections caused by PLHIV
 # beta option 2: the number of infections occurring in susceptible people
 if (projectspecs$betaoption == "N") {
-  
-  beta0<- betaoption1(parameters = parameters_beta0)
+beta0<- betaoption1(parameters = parameters_beta0)
 } else if (projectspecs$value[5] == "S") {
-  beta0<- betaoption2(parameters = parameters_beta0)
+beta0<- betaoption2(parameters = parameters_beta0)
 } else {beta0 <- 0}
-
 # After obtain beta0, Put parmaters as vectors for HIVmodel
 parameters<- c(beta0 =beta0,
-               N0=fixeddata$value[fixeddata$Para == "plhiv"], ##plhiv
-               I0=fixeddata$value[fixeddata$Para == "newinf"], ##new infection
-               d0=fixeddata$value[fixeddata$Para == "prodiag"], ##proportion  diagnosed
-               tau0=fixeddata$value[fixeddata$Para == "proART"], ##proportion on ART
-               sigma0=fixeddata$value[fixeddata$Para == "prosupp"] ,##proportion suppressed
-               phi=defaultpara$value[defaultpara$Para == "effisupp"],  ##the reduction in transmission due to suppression 0.95?
-               omega0= fixeddata$value[fixeddata$Para == "PrEPcov"], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
-               epsilon_PrEP=defaultpara$value[defaultpara$Para == "effiPrEP"],  ###scott(0.86) efficacy of PrEP
-               epsilon_condom=defaultpara$value[defaultpara$Para == "efficon"],  ###efficacy condoms, scott 
-               chi_0=fixeddata$value[fixeddata$Para == "conduse"], ##proportion of condom use FIG21 HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
-               alpha0=fixeddata$value[fixeddata$Para == "sexact"],  ####proportion of sexual acts (baseline) Table1 Hammoud, total number of partners mean
-               T= fixeddata$value[fixeddata$Para == "testpro"])##proportion of testing, FIG22a HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
+N0=fixeddata$value[fixeddata$Para == "plhiv"], ##plhiv
+I0=fixeddata$value[fixeddata$Para == "newinf"], ##new infection
+d0=fixeddata$value[fixeddata$Para == "prodiag"], ##proportion  diagnosed
+tau0=fixeddata$value[fixeddata$Para == "proART"], ##proportion on ART
+sigma0=fixeddata$value[fixeddata$Para == "prosupp"] ,##proportion suppressed
+phi=defaultpara$value[defaultpara$Para == "effisupp"],  ##the reduction in transmission due to suppression 0.95?
+omega0= fixeddata$value[fixeddata$Para == "PrEPcov"], ##PrEP coverage (FIG1 Hammoud/##FIG21 HIV Strategy 2016 ?C 2020)
+epsilon_PrEP=defaultpara$value[defaultpara$Para == "effiPrEP"],  ###scott(0.86) efficacy of PrEP
+epsilon_condom=defaultpara$value[defaultpara$Para == "efficon"],  ###efficacy condoms, scott
+chi_0=fixeddata$value[fixeddata$Para == "conduse"], ##proportion of condom use FIG21 HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
+alpha0=fixeddata$value[fixeddata$Para == "sexact"],  ####proportion of sexual acts (baseline) Table1 Hammoud, total number of partners mean
+T= fixeddata$value[fixeddata$Para == "testpro"])##proportion of testing, FIG22a HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
 #the monthly data are extracted. (below are monthly data from Jan 2020 - Dec 2020)
 temporal_data<-  as.data.frame(  cbind(alpha_t=c(timeseries$acts),###Table1 Hammoud
-                                     chi_t=c(timeseries$condoms), ##FIG21 NSW HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
-                                     T_p= c(timeseries$testing),###testing
-                                     ##FIG22a NSW HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
-                                     omega_p=c(timeseries$prep),###PREP
-                                     IM=c(timeseries$immigrants),
-                                     EM=c(timeseries$emigrants),
-                                     Death=c(timeseries$Deaths),
-                                     tau=c(timeseries$tau),
-                                     sigma=c(timeseries$sigma))) ###migrants month 0-12
-
-
-return(as.list(c(parameters, temporal_data))) 
-  })
+chi_t=c(timeseries$condoms), ##FIG21 NSW HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
+T_p= c(timeseries$testing),###testing
+##FIG22a NSW HIV Strategy 2016 ?C 2020 Quarter 4 & Annual 2020 Data Report
+omega_p=c(timeseries$prep),###PREP
+IM=c(timeseries$immigrants),###migrants month 0-12
+EM=c(timeseries$emigrants),
+Death=c(timeseries$Deaths),
+tau=c(timeseries$tau),
+sigma=c(timeseries$sigma)))
+return(as.list(c(parameters, temporal_data)))
+})
 }
-
--- code/SimpleHiv.R
+++ code/SimpleHiv.R
@@ -1,63 +1,59 @@
 ####This script is for the SIMPLEHIV model.
-#'The model aims to describe the effect of monthly changes in sexual 
-#'partnerships, condom use, HIV testing, PrEP use on HIV transmission. 
+#'The model aims to describe the effect of monthly changes in sexual
+#'partnerships, condom use, HIV testing, PrEP use on HIV transmission.
 #'It captures the key stages of the HIV cascade
-
-
 ######Basic equation##########
 #'Estimate number of new infections due to transmission from PWHIV to susceptible sexual partners:
-#'I=N¦Â(1-d¦Ó¦Ò??)(1-¦Ø??_p),
-#'were N is the number of PWHIV in the population, ¦Â is the transmission 
-#'probability for an unsuppressed PWHIV to a susceptible partner not on PrEP, 
-#'d is the proportion of PWHIV diagnosed, ¦Ó(tau) is the proportion diagnosed on ART, 
-#'¦Ò(sigma) is the proportion on ART with suppressed virus (< 200 copies per ml), 
-#'??(phi) is the reduction in transmission due to suppression (??¡Ö0.95), 
-#'¦Ø(omega) is the proportion of susceptible GBMSM on PrEP, 
+#'I=NÎ²(1-dÏ„Ïƒ??)(1-Ï‰??_p),
+#'were N is the number of PWHIV in the population, Î² is the transmission
+#'probability for an unsuppressed PWHIV to a susceptible partner not on PrEP,
+#'d is the proportion of PWHIV diagnosed, Ï„(tau) is the proportion diagnosed on ART,
+#'Ïƒ(sigma) is the proportion on ART with suppressed virus (< 200 copies per ml),
+#'??(phi) is the reduction in transmission due to suppression (??â‰ˆ0.95),
+#'Ï‰(omega) is the proportion of susceptible GBMSM on PrEP,
 #'and ??_p(epsilon_PrEP) is the efficacy of PrEP in preventing infection.
-
 ######Basic equation with timesteps##########
-#'This equation can be turned into a time varying equation and solved use the 
+#'This equation can be turned into a time varying equation and solved use the
 #'following recurrence equation for each time-step t:
-#' N(t+1)= N(t)+I(t)+M(t), 
+#' N(t+1)= N(t)+I(t)+M(t),
 #' #'M(t)=IM(t)-EM(t) the number of immigrate - the number of emigrate
 #' IM(t) is the number of GBMSM who enter Australia already
 #' infected with HIV (which will essentially stop due to border closures)
 #' EM(t) is the number of GBMSM who leave Australia already
 #' infected with HIV (which will essentially stop due to border closures)
-  
 ######The change of Beta(t)#######
 #'Effect of changing condom use and sexual partners
-#'Relative change in incidence if condom use changes to c_t(chi_t) 
+#'Relative change in incidence if condom use changes to c_t(chi_t)
 #'compared to a basic level of c_0(chi_0)
 #' (1-??c_t)/(1-??c_0 ). ??(epsilon_condom) is the efficacy of condom use in preventing infection
 #' Relative change in incidence if the number of sexual acts in a time-step changes
 #' to a_t(alpha_t) compared to a basic level of a_0(alpha0): a_t/a_0.
-#'The number of acts is given by number of partnerships multiplied by the number of acts per partnership. 
+#'The number of acts is given by number of partnerships multiplied by the number of acts per partnership.
 #'Combining these relative changes means we can write:
-#'  ¦Â(t)= ¦Â(0)*(alpha_t/alpha0)((1-epsilon_condom*chi_t*chi_0)/(1-epsilon_condom*chi_0)).
-
+#'  Î²(t)= Î²(0)*(alpha_t/alpha0)((1-epsilon_condom*chi_t*chi_0)/(1-epsilon_condom*chi_0)).
 ######The change in testing rates######
-#'The proportion diagnosed is affected over time by the change in testing rates 
-#'(which are affected by external factors such as COVID restrictions) if the proportion of GBMSM testing 
-#'during time-step t is T(t) the number of PWHIV diagnosed at the next time step is: 
+#'The proportion diagnosed is affected over time by the change in testing rates
+#'(which are affected by external factors such as COVID restrictions) if the proportion of GBMSM testing
+#'during time-step t is T(t) the number of PWHIV diagnosed at the next time step is:
 #'  N(t+1) *d(t+1) = N(t)* d(t)+I(t) *T(t)+(1-d(t) ) *N(t)* T(t),
-#'or 
+#'or
 #'d(t+1 )=  (N(t)* d(t)+[I(t)+(1-d(t) )* N(t) ] *T(t))/(N(t)+I(t)+M(t) ).
-#'T(t) =T*T_p 
+#'T(t) =T*T_p
 #'T is the basic testing rates and T_p is the relative changes of testing rates with timesteps
-
-SimpleHIV<-function(times,state, parameters){  
-  with(as.list(c(state, parameters)), {
-       for(i in 1:times)
-  {# The equations
-    I[i]=N[i]*beta0*
-      (alpha_t[i]/alpha0)*
-      ((1-epsilon_condom*chi_t[i]*chi_0)/(1-epsilon_condom*chi_0))*
-      ((1-d[i]*tau[i]*sigma[i]*phi)*(1-omega0*omega_p[i]*epsilon_PrEP))
-    N[i+1]=(N[i]+I[i]+IM[i]-EM[i])*(1-Death[i])
-    d[i+1]=(N[i]*d[i]+(I[i] +(1-d[i])*N[i])*T*T_p[i])/((N[i]+I[i]+IM[i]-EM[i])*(1-Death[i]))}
-    returnedList<-list(I, N)
-    names(returnedList) <- c("I", "N")
-    return(returnedList) 
-    })
+SimpleHIV<-function(times,state, parameters){
+with(as.list(c(state, parameters)), {
+diagnosis<-c()
+for(i in 1:times)
+{# The equations
+I[i]=N[i]*beta0*
+(alpha_t[i]/alpha0)*
+((1-epsilon_condom*chi_t[i]*chi_0)/(1-epsilon_condom*chi_0))*
+((1-d[i]*tau[i]*sigma[i]*phi)*(1-omega0*omega_p[i]*epsilon_PrEP))
+diagnosis[i]=(I[i] +(1-d[i])*N[i])*T*T_p[i]
+N[i+1]=(N[i]+I[i]+IM[i]-EM[i])*(1-Death[i])
+d[i+1]=(N[i]*d[i]+(I[i] +(1-d[i])*N[i])*T*T_p[i])/((N[i]+I[i]+IM[i]-EM[i])*(1-Death[i]))}
+returnedList<-list(I, N,d,diagnosis)
+names(returnedList) <- c("I", "N","d","diagnosis")
+return(returnedList)
+})
 }
